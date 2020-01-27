750306b733d25cebc28837ec96f9bbda53f31e24
Steven Perfors <stevecanbuild@gmail.com>
2020-01-27 16:54:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if small_value == 0 || value < tiny_key
       small_value = value 
-      
+      tiny_key = key 
     
 end
 
